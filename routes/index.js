612f74d18b8d869c222b5b1a143740d4fe302f65
Ian Salmon <isalmon1@gmail.com>
2019-07-23 20:12:27 -0600

    OOPS

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'I <3 AWS' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'I <3 AWS' });
+// });
 
-module.exports = router;
+// module.exports = router;
